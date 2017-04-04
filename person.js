a0462a15514552fee860f2ef08bd3fc1d1465aba
Mike Ziccardi <mikeziccardi@gmail.com>
2017-04-04 08:52:52 -0600

    word

--- person.js
+++ person.js
@@ -1,8 +1,8 @@
 export default class Person {
   constructor() {
-    // each person starts out with 5 dollars
+    // each person starts out with 5 dollars!
     this.state = {
-      credits = 500      
+      credits = 500
     }
   }
 }
